951c31f42387d14e1e3569e0b050f54a691885d9
ventsy95 <ventsy95@mail.bg>
2016-10-02 22:13:09 +0300

    Added extra exception throwing in constructor for better exception tracking. Removed unused method for password validation + unused constants.

--- src/main/java/bg/piggybank/model/ContactInfo.java
+++ src/main/java/bg/piggybank/model/ContactInfo.java
@@ -3,10 +3,6 @@ package bg.piggybank.model;
 import bg.piggybank.model.exeptions.*;
 
 public abstract class ContactInfo {
-//	private static final int MAX_LENGTH_LONG_NUMBER = 13;
-//	private static final String HOW_TO_START_LONG_NUMBER = "+3598";
-//	private static final int MAX_LENGTH_SHORT_NUMBER = 10;
-//	private static final String HOW_TO_START_NUMBER = "08";
 	private String address;
 	private String city;
 	private String country;
@@ -14,16 +10,31 @@ public abstract class ContactInfo {
 	private String email;
 
 	public ContactInfo(String address, String city, String country, String phoneNum, String email) throws IncorrectContactInfoException {
-			if (isValid(address) && isValid(city) && isValid(country) && isValidEmailAddress(email)) {
-			Checkers.isPhoneNumberValid(phoneNum);
-				this.address = address;
-				this.city = city;
-				this.country = country;
-				this.phoneNum = phoneNum;
-				this.email=email;
-			} else {
-				throw new IncorrectContactInfoException("Incorrect value for contact info");
-			}
+		if (isValid(address)) {
+			this.address = address;
+		} else {
+			throw new IncorrectContactInfoException("Incorrect value for address");
+		}
+		if (isValid(city)) {
+			this.city = city;
+		} else {
+			throw new IncorrectContactInfoException("Incorrect value for city");
+		}
+		if (isValid(country)) {
+			this.country = country;
+		} else {
+			throw new IncorrectContactInfoException("Incorrect value for country");
+		}
+		if (Checkers.isPhoneNumberValid(phoneNum)) {
+			this.phoneNum = phoneNum;
+		} else {
+			throw new IncorrectContactInfoException("Incorrect value for phone number");
+		}
+		if (isValidEmailAddress(email)) {
+			this.email = email;
+		} else {
+			throw new IncorrectContactInfoException("Incorrect value for email");
+		}
 	}
 
 	public boolean isValid(String string) {
@@ -33,18 +44,6 @@ public abstract class ContactInfo {
 			return false;
 		}
 	}
-
-//	private boolean isValidPhoneNumber(String telephone) {
-//		boolean isValidNum = false;
-//		if (isValid(telephone)) {
-//			if ((telephone.substring(0, 2).equals(HOW_TO_START_NUMBER) && telephone.length() == MAX_LENGTH_SHORT_NUMBER)
-//					|| (telephone.substring(0, 5).equals(HOW_TO_START_LONG_NUMBER)
-//							&& telephone.length() == MAX_LENGTH_LONG_NUMBER)) {
-//				isValidNum = true;
-//			}
-//		}
-//		return isValidNum;
-//	}
 
 	private boolean isValidEmailAddress(String email) {
 		String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
