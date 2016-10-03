7ed00e79d5c0d04a59f9b5284b16b8d7eee05aea
ventsy95 <ventsy95@mail.bg>
2016-10-03 20:53:08 +0300

    DAO configuration class for autowiring all DAOs' added.

---
+++ src/main/java/bg/piggybank/model/configurations/DAOConfig.java
@@ -0,0 +1,8 @@
+package bg.piggybank.model.configurations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages= "bg.piggybank")
+public class DAOConfig {}
