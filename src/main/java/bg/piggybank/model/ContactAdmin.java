8c043a322865c4aa26af9fd9319ea285ef0c9e74
Nikolina Doycheva <enkeif@abv.bg>
2016-10-14 15:23:56 +0300

    edited method for sending emails + validation

--- src/main/java/bg/piggybank/model/ContactAdmin.java
+++ src/main/java/bg/piggybank/model/ContactAdmin.java
@@ -12,45 +12,48 @@ import javax.mail.internet.MimeMessage;
 
 public class ContactAdmin {
 	
-	public void sendEmail(String senderName, String email, String description) {
+            public void sendEmail(String senderName, String email, String description) {
+		if (isValid(senderName) && isValid(email) && isValid(description)) {
 
-		setSenderName(senderName);
-		setEmail(email);
-		setDescription(description);
-		
-		final String username = "piggycontact.system@gmail.com";
-		final String password = "PiggyBank";
+			final String username = "piggycontact.system@gmail.com";
+			final String password = "PiggyBank";
 
-		Properties props = new Properties();
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.port", "587");
+			Properties props = new Properties();
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.starttls.enable", "true");
+			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.put("mail.smtp.port", "587");
 
-		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, password);
+			Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			});
+
+			try {
+
+				Message message = new MimeMessage(session);
+				message.setFrom(new InternetAddress("piggycontact.system@gmail.com"));
+				message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("piggybank@abv.bg"));
+				message.setSubject("New message from user " + senderName);
+				message.setText("User's e-mail: " + email + " \n " + description);
+
+				Transport.send(message);
+
+				System.out.println("Done");
+
+			} catch (MessagingException e) {
+				e.printStackTrace();
 			}
-		});
-
-		try {
-
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("piggycontact.system@gmail.com"));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("piggybank@abv.bg"));
-			message.setSubject("New message from user " + senderName);
-			message.setText("User's e-mail: " + email + " \n " + description);			
-
-			Transport.send(message);
-
-			System.out.println("Done");
-
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
 		}
 	}
 
-
-
+	private boolean isValid(String string) {
+		if (string != null && !string.trim().equals("")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 }
