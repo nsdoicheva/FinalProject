6d3148f52a24f95aafbd8b66a2796b9370765beb
ventsy95 <ventsy95@mail.bg>
2016-10-16 14:11:12 +0300

    AmountDAO class update.

--- src/main/java/bg/piggybank/model/amounts/AmountDAO.java
+++ src/main/java/bg/piggybank/model/amounts/AmountDAO.java
@@ -1,4 +1,5 @@
 package bg.piggybank.model.amounts;
+
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,20 +11,25 @@ import bg.piggybank.model.DBConnection;
 import bg.piggybank.model.accounts.Account;
 import bg.piggybank.model.accounts.AccountDAO;
 import bg.piggybank.model.exeptions.FailedConnectionException;
+import bg.piggybank.model.user.User;
+import bg.piggybank.model.user.UserDAO;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Timestamp;
 
 @Component
 public class AmountDAO {
-	private static final String SELECT_ALL_MY_AMOUNTS = "SELECT id, date, money, account_id FROM amounts;";
-	private static final String SELECT_ALL_ACCOUNT_AMOUNTS = "SELECT id, date, money, account_id FROM amounts WHERE account_id=?;";
+	private static final String SELECT_AMOUNTS = "SELECT id, date, money, account_id FROM amounts WHERE account_id=?;";
+
 	@Autowired
 	private AccountDAO accountDao;
+	@Autowired
+	private UserDAO userDao;
+
 	private static volatile int count = 0;
-	
+
 	public void startAmountTrackingAfterServerRestart() {
 		count++;
 		if (count <= 1) {
@@ -36,23 +42,29 @@ public class AmountDAO {
 			return;
 		}
 	}
-	
-	public List<Amount> getAllMyAmounts(){
+
+	public List<Amount> getAllMyAmounts(String username) {
 		Connection connection;
-		List<Amount> amounts= new ArrayList<Amount>();
+		List<Amount> amounts = new ArrayList<Amount>();
 		try {
-			connection= DBConnection.getInstance().getConnection();
-			Statement statement= connection.createStatement();
-			ResultSet result= statement.executeQuery(SELECT_ALL_MY_AMOUNTS);
-			while(result.next()){
-				int id=result.getInt("id");
-				Timestamp date= result.getTimestamp("date");
-				double money= result.getDouble("money");
-				int accountId= result.getInt("account_id");
-				Account account = accountDao.getAccountByID(accountId, connection);
-				String IBAN= Account.decryptIban(account.getIBAN());
-				Amount amount= new Amount(id, date, money, IBAN);
-				amounts.add(amount);
+			connection = DBConnection.getInstance().getConnection();
+			User user = userDao.getUserByUsername(username);
+			List<Integer> accountIds = accountDao.getAccountIDByUserID(user.getId());
+			for (int accountID : accountIds) {
+				PreparedStatement statement = connection.prepareStatement(SELECT_AMOUNTS);
+				statement.setInt(1, accountID);
+				ResultSet result = statement.executeQuery();
+				while (result.next()) {
+					int id = result.getInt("id");
+					Timestamp date = result.getTimestamp("date");
+					double money = result.getDouble("money");
+					int accountId = result.getInt("account_id");
+					Account account = accountDao.getAccountByID(accountId, connection);
+					String IBAN = account.getIBAN();
+					System.out.println(IBAN);
+					Amount amount = new Amount(id, date, money, IBAN);
+					amounts.add(amount);
+				}
 			}
 		} catch (FailedConnectionException | SQLException e) {
 			e.printStackTrace();
@@ -60,22 +72,21 @@ public class AmountDAO {
 		}
 		return amounts;
 	}
-	
-	public List<Amount> getAllAmountsForAccount(String IBAN){
+
+	public List<Amount> getAllAmountsForAccount(String IBAN) {
 		Connection connection;
-		List<Amount> amounts= new ArrayList<Amount>();
+		List<Amount> amounts = new ArrayList<Amount>();
 		try {
-			connection= DBConnection.getInstance().getConnection();
-			PreparedStatement statement= connection.prepareStatement(SELECT_ALL_ACCOUNT_AMOUNTS);
-			statement.setString(1, Account.cryptIban(IBAN));
-			ResultSet result= statement.executeQuery();
-			while(result.next()){
-				int id=result.getInt("id");
-				Timestamp date= result.getTimestamp("date");
-				double money= result.getDouble("money");
-				int accountId= result.getInt("account_id");
-				Account account = accountDao.getAccountByID(accountId, connection);
-				Amount amount= new Amount(id, date, money, IBAN);
+			connection = DBConnection.getInstance().getConnection();
+			Account account = accountDao.getAccountByIBAN(IBAN);
+			PreparedStatement statement = connection.prepareStatement(SELECT_AMOUNTS);
+			statement.setInt(1, account.getId());
+			ResultSet result = statement.executeQuery();
+			while (result.next()) {
+				int id = result.getInt("id");
+				Timestamp date = result.getTimestamp("date");
+				double money = result.getDouble("money");
+				Amount amount = new Amount(id, date, money, IBAN);
 				amounts.add(amount);
 			}
 		} catch (FailedConnectionException | SQLException e) {
