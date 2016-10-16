a99eec05d65919e2216825ae147f0bf88662c961
ventsy95 <ventsy95@mail.bg>
2016-10-16 09:40:46 +0300

    Unit tests added.

--- src/test/java/bg/piggybank/PiggyBankApplicationTests.java
+++ src/test/java/bg/piggybank/PiggyBankApplicationTests.java
@@ -1,7 +1,6 @@
 package bg.piggybank;
 
 import java.sql.SQLException;
-import java.util.Set;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import bg.piggybank.model.DataToTable;
+import bg.piggybank.model.DBConnection;
 import bg.piggybank.model.accounts.Account;
 import bg.piggybank.model.accounts.AccountDAO;
 import bg.piggybank.model.accounts.AccountType;
 import bg.piggybank.model.accounts.CurrencyType;
-import bg.piggybank.model.configurations.DAOConfig;
+import bg.piggybank.model.amounts.AmountDAO;
+import bg.piggybank.model.amounts.AmountSaver;
+import bg.piggybank.model.configurations.Config;
 import bg.piggybank.model.exeptions.AccountException;
 import bg.piggybank.model.exeptions.FailedConnectionException;
 import bg.piggybank.model.exeptions.IncorrectContactInfoException;
@@ -26,7 +27,7 @@ import bg.piggybank.model.user.UserDAO;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes= DAOConfig.class)
+@ContextConfiguration(classes= Config.class)
 public class PiggyBankApplicationTests {
 	
 	@Autowired
@@ -35,21 +36,27 @@ public class PiggyBankApplicationTests {
 	private AccountDAO accountDAO;
 	@Autowired
 	private TransactionDAO transactionDAO;
-
+	@Autowired
+	private AmountDAO amountDAO;
+	
 	/*@Test
 	public void testUserDao() throws IncorrectContactInfoException{
 		userDAO.saveUser(new User("Ivancho", "Ivan123", "ivancho", "doma", "Sofiq", "Bulgaria", "0894235689", "ivancho@mail.bg", "9608222836", "siganin"));
-	}
-	
-	@Test
-	public void testAccountDao() throws UserInfoException, AccountException, SQLException, FailedConnectionException, IncorrectContactInfoException{
-		accountDAO.registrateUserAccount(new User("Ivancho", "Ivan123", "ivancho", "doma", "Sofiq", "Bulgaria", "0894235689", "ivancho@mail.bg", "9608222836", "siganin"), new Account("smetka", AccountType.RAZPLASHTATELNA, CurrencyType.USD, 3000));
-		System.out.println(accountDAO.getNumberOfAccounts());
 	}*/
 	
+	/*@Test
+	public void testAccountDao() throws UserInfoException, AccountException, SQLException, FailedConnectionException, IncorrectContactInfoException{
+		User user = new User("Ivancho", "Ivan123", "ivan123", "doma", "Sofiq", "Bulgaria", "0894835689", "ivancho8@mail.bg", "9308222836", "siganin");
+		userDAO.saveUser(user);
+		Account account= new Account("smetka", AccountType.RAZPLASHTATELNA, CurrencyType.USD, 3000);
+		accountDAO.registrateUserAccount(user , account);
+		System.out.println(AccountDAO.getNumberOfAccounts());
+		System.out.println(account.getIBAN());
+	}*/
+	/*
 	@Test
 	public void testTransactionDao() throws IncorrectContactInfoException, UserInfoException, AccountException, SQLException, FailedConnectionException{
-		/*DataToTable.fillCurrencies();
+		DataToTable.fillCurrencies();
 		DataToTable.fillAccountTypes();
 		User vancho=new User("Ivancho", "Ivan123", "ivancho", "doma", "Sofiq", "Bulgaria", "0894235689", "ivancho@mail.bg", "9608222836", "siganin");
 		userDAO.saveUser(vancho);
@@ -57,10 +64,42 @@ public class PiggyBankApplicationTests {
 		accountDAO.registrateUserAccount(vancho, from);
 		Account to= new Account("vtora smetka", AccountType.RAZPLASHTATELNA, CurrencyType.USD, 3000);
 		accountDAO.registrateUserAccount(vancho, to);
-		transactionDAO.saveTransaction(from, to, 600, "na ti zaplata");*/
+		transactionDAO.saveTransaction(from, to, 600, "na ti zaplata");
 		for(Transaction transaction: transactionDAO.listAllTransactions()){
 			System.out.println(transaction);
 		}
+	}*/
+	
+	/*@Test
+	public void testLoginUser(){
+		userDAO.loginUser("nikolina", "Nikolina95");
+		System.out.println(userDAO.getUserByUsername("nikolina"));
+	}*/
+	/*
+	@Test
+	public void testIBANCriptation(){
+		String iban= "BG02BNBG45452011000067";
+		String cripted= Account.cryptIban(iban);
+		System.out.println(Account.cryptIban(iban));
+		System.out.println(cripted);
+		String decripted= Account.decryptIban(cripted);
+		System.out.println(decripted);
+	}*/
+	
+	/*@Test
+	public void testGetAccountTypes(){
+		System.out.println(accountDAO.getAllAccountTypes());
+	}*/
+	
+	/*@Test
+	public void testAmountSaver(){
+		Thread amountSaver= new Thread(new AmountSaver(1, accountDAO));
+		//amountSaver.start();
+		amountSaver.run();
+	}*/
+	
+	@Test
+	public void testSaverAfterServerStop(){
+		amountDAO.startAmountTrackingAfterServerRestart();
 	}
-
-}
+  }
