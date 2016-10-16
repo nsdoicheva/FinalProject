d704b085a7efdecc643d40347640608c5c0d3b0b
ventsy95 <ventsy95@mail.bg>
2016-10-16 14:13:16 +0300

    Class, containing constants created.

---
+++ src/main/java/bg/piggybank/model/Constants.java
@@ -0,0 +1,16 @@
+package bg.piggybank.model;
+
+public abstract class Constants {
+	public static final int MINIMAL_USERNAME_LENGTH = 3;
+	public static final int MAXIMAL_USERNAME_LENGTH = 15;
+	public static final String WRONG_PASSWORD = "@@WRONGpASSwORD@@";
+	public static final int MINIMAL_PASSWORD_LENGTH = 8;
+	public static final int IBAN_LENGTH = 22;
+	public static final int START_POSITION_OF_ACCOUNT_NUMBER = 13;
+	public static final int VISA_CARD_IDENTIFY = 4;
+	public static final int VISA_CARD_RANGE = 10;
+	public static final int BANK_ID_NUMBER = 4545;
+	public static final int ALGORITHM_OF_LUHM_CONSTANT = 10;
+	public static final int ALGORITHM_OF_LUHM_KEY = 9;
+	public static final String CARD_TYPE = "Bacon";
+}
