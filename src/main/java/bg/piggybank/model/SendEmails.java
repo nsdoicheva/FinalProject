73c2b0888a3af9827a34360eb2818a3b510d29ed
ventsy95 <ventsy95@mail.bg>
2016-10-16 09:39:36 +0300

    Class for sending emails added.

--- src/main/java/bg/piggybank/model/SendEmails.java
+++ src/main/java/bg/piggybank/model/SendEmails.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -14,39 +16,27 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import bg.piggybank.model.exeptions.FailedConnectionException;
-
+import bg.piggybank.model.transactions.Transaction;
 
 public class SendEmails implements Runnable {
-	
-	private static final String LAST_EMAIL = "Select email from users where id = (SELECT MAX(ID) FROM users); ";
-	private String receiver;
-	
+	private List<String> emails;
+
 	public SendEmails() {
-		setReceiver(receiver);
+		this.emails = new LinkedList<String>();
 	}
 
-	@Override
-	public void run() {
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			return;
-		}		
-		String user = lastEmail();
-		sendEmail(user, receiver);		
-		
-	}
-	
 	public String lastEmail() {
 		String result = null;
 		try {
 			Connection connection = DBConnection.getInstance().getConnection();
-			PreparedStatement ps = connection.prepareStatement(LAST_EMAIL);
+			PreparedStatement ps = connection.prepareStatement("Select email from users;");
 			ResultSet resultSet = ps.executeQuery();
 
-			if (resultSet.next()) {
-				result = resultSet.getString("email");
+			while (resultSet.next()) {
+				emails.add(resultSet.getString("email"));
 			}
+
+			result = emails.get(emails.size() - 1);
 
 		} catch (SQLException e) {
 			System.out.println("User cannot be logged right now.");
@@ -55,57 +45,53 @@ public class SendEmails implements Runnable {
 		}
 		return result;
 	}
-	
 
-	public void sendEmail(String email, String user) {
+	public void sendEmail(String email) {
 
-		if (isValid(email) && isValid(user)) {
+		final String username = "nsdoicheva@gmail.com";
+		final String password = "nikolinaeqka";
 
-			final String username = "piggycontact.system@gmail.com";
-			final String password = "PiggyBank";
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
 
-			Properties props = new Properties();
-			props.put("mail.smtp.auth", "true");
-			props.put("mail.smtp.starttls.enable", "true");
-			props.put("mail.smtp.host", "smtp.gmail.com");
-			props.put("mail.smtp.port", "587");
-
-			Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(username, password);
-				}
-			});
-
-			try {
-				Message message = new MimeMessage(session);
-				message.setFrom(new InternetAddress("piggycontact.system@gmail.com"));
-				message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
-				message.setSubject("New registered user!");
-				message.setText("Welcome to PiggyBank, " + user + "!");
-
-				Transport.send(message);
-
-				System.out.println("Done");
-
-			} catch (MessagingException e) {
-				e.printStackTrace();
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
 			}
+		});
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("nsdoicheva@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+			message.setSubject("New registered user!");
+			message.setText("Welcome to PiggyBank!");
+
+			Transport.send(message);
+
+			System.out.println("Done");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
 		}
 	}
 
-
-	public void setReceiver(String receiver) {
-		if (isValid(receiver)) {
-			this.receiver = receiver;
+	@Override
+	public void run() {
+		int size = emails.size();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			return;
 		}
-	}
 
-	public boolean isValid(String string) {
-		if (string != null && !string.trim().equals("")) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+		// if emails.size() - size == 1
+		String user = lastEmail();
+		sendEmail(user);
 
+	}
 }
